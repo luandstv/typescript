130d6336ef88435f1c035c3f93b6236d58c75c02
Luan Da Silva Alves - LAE <luan.alves@iteris.com.br>
2022-06-29 18:05:45 -0300

    parte 1 curso alura

---
+++ app/app.ts
@@ -0,0 +1,8 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controler = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controler.adiciona()
+});
